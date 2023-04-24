8dd701f9fe46c281f0f3099207b565da4d0a7b0c
yendor87 <rodneydavidmaciel@gmail.com>
2023-04-24 09:27:30 -0400

    header added

---
+++ js/menu.js
@@ -0,0 +1,7 @@
+let logo = document.querySelector(".w-content");
+let menu = document.querySelector(".menu");
+logo.addEventListener("click", () => {
+    menu.classList.toggle("showmenu");
+})
+
+// https://www.youtube.com/watch?v=qoULWGhiC0w
